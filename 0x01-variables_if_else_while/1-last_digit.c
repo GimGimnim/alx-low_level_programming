54ca5d4281455c7d123c8fedbf715a461f691900
GimGimnim <fabekuolukayero@gmail.com>
2022-09-09 09:11:07 -0700

    Greatest

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,10 +15,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	printf("Last digit of %d is %d", n, n % 10);
 	if (n % 10 > 5)
-		printf(" and is greater than 5");
+		printf(" and is greater than %d", n = 5);
 	else if (n % 10 == 0)
-		printf(" and is 0");
+		printf(" and is %d", n = 0);
 	else
-		printf(" and is less than 6 and not 0");
+		printf(" and is less than %d and not %d", 6, 0);
 	return (0);
 }
