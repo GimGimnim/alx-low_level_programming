b080b1772140c027fb5163eefb9f8818a59d29e4
GimGimnim <111288662+GimGimnim@users.noreply.github.com>
2022-09-15 13:48:33 +0100

    G

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,7 +1,29 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_triangle - Prints a triangle
+ * @size: Size of triangle (in lines)
  */
+void print_triangle(int size)
+{
+	int x, y;
+
+	if (size < 1)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (y = 0; y < size; y++)
+	{
+		for (x = 1; x <= size; x++)
+		{
+			if (size - x <= y)
+				_putchar('#');
+			else
+				_putchar(' ');
+		}
+
+		_putchar('\n');
+	}
+}
