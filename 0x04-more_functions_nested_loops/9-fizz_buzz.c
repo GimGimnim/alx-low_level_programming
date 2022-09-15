86fdce93f7a81be1ab7baa450c795199fbdf1cab
GimGimnim <fabekuolukayero@gmail.com>
2022-09-15 07:24:52 -0700

    Greatest

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,26 +10,26 @@ int main(void)
 {
 	int i;
 
-	while (i = 1, i < 100, i++)
+	for (i = 1, i < 101, i++)
 	{
-		if (i % 3 == 0)
+		if (i % 15 == 0)
 		{
-			_putchar('Fizz');
+			printf("FizzBuzz");
 			_putchar(' ');
 		}
 		else if (i % 5 == 0)
 		{
-			_putchar('Buzz');
+			printf("Buzz");
 			_putchar(' ');
 		}
-		else if (1 % 15 == 0)
+		else if (i % 3 == 0)
 		{
-			_putchar('FizzBuzz');
+			printf("Fizz");
 			_putchar(' ');
 		}
 		else
 		{
-			_putchar('i');
+			printf("%d", i);
 			_putchar(' ');
 		}
 	}
