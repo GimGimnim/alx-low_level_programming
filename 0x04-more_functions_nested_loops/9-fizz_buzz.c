8df40d5849b69370f48dde4ad7b94f171c3414af
GimGimnim <111288662+GimGimnim@users.noreply.github.com>
2022-09-15 15:37:21 +0100

    Greatest

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -9,30 +10,21 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 101; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
-		{
-			printf("FizzBuzz");
-			putchar(' ');
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz");
-			putchar(' ');
-		}
-		else if (i % 3 == 0)
-		{
+		if (i % 3 == 0)
 			printf("Fizz");
-			putchar(' ');
-		}
-		else
-		{
+
+		if (i % 5 == 0)
+			printf("Buzz");
+
+		if (i % 5 != 0 && i % 3 != 0)
 			printf("%d", i);
+
+		if (i < 100)
 			putchar(' ');
-		}
 	}
 
 	putchar('\n');
-	return(0);
+	return (0);
 }
