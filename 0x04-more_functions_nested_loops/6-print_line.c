ffdd9af097451ab3ab7d41c4b1d666c84dcffeda
GimGimnim <111288662+GimGimnim@users.noreply.github.com>
2022-09-15 13:55:10 +0100

    good

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,7 +1,24 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_line - Draws a straight line in the terminal
+ * @n: length of the line (in characters)
  */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+
+	_putchar('\n');
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+St
