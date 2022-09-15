d7d01c3ef5806b5c6b977603326844e841e6d35e
GimGimnim <111288662+GimGimnim@users.noreply.github.com>
2022-09-15 13:57:23 +0100

    good

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,26 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_square - Prints a square
+ * @size: Size of the square to be printed
  */
+void print_square(int size)
+{
+	int x, y;
+
+	if (size < 1)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (y = 0; y < size; y++)
+	{
+		for (x = 0; x < size; x++)
+		{
+			_putchar('#');
+		}
+
+		_putchar('\n');
+	}
+}
