adf8dd7ab6aa523176d6500d2ef6814284f3c585
GimGimnim <fabekuolukayero@gmail.com>
2022-09-14 17:51:09 -0700

    Greatest

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,23 +1,7 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <time.h>
 
-int main(void)
-{
-	int i;
-
-	srand(time(0));
-	i = rand() - RAND_MAX / 2;
-
-	return (int i);
-}
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_days(int month, int day);
-
-#endif /* MAIN_H */
