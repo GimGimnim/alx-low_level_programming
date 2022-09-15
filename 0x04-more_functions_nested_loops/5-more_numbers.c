eaaae7092957af481195162cd31e099c18e81c70
GimGimnim <111288662+GimGimnim@users.noreply.github.com>
2022-09-15 13:54:39 +0100

    good

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,21 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * more_numbers - Prints the numbers 0 to 14, 10 times
  */
+void more_numbers(void)
+{
+	int x, y;
+
+	for (y = 0; y < 10; y++)
+	{
+		for (x = 0; x < 15; x++)
+		{
+			if (x > 9)
+				_putchar('0' + x / 10);
+			_putchar('0' + x % 10);
+		}
+
+		_putchar('\n');
+	}
+}
