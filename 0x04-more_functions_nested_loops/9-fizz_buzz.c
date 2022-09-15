1abf6b040878c668da9159d5303df04b2e2275a8
GimGimnim <fabekuolukayero@gmail.com>
2022-09-15 07:06:05 -0700

    Greatest

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,40 @@
 #include <stdio.h>
-
+#include "main.h"
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
+  int main(void)
+{
+	int i;
+	char c
+
+	i = 1;
+
+	while(i = 1, i < 100, i++)
+	{
+		if(i % 3 == 0)
+		{
+			_putchar('Fizz');
+			_putchar(' ');
+		}
+		else if(i % 5 == 0)
+		{
+			_putchar('Buzz');
+			_putchar(' ');
+		}
+		else if(1 % 15 == 0)
+		{
+			_putchar('FizzBuzz');
+			_putchar(' ');
+		}
+		else
+		{
+			_putchar('i');
+			_putchar(' ');
+		}
+	}
+
+	return (0);
+}
