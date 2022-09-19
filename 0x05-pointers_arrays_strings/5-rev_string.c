af7f922fdfeb723751bf413eefab5cd6a99ff5ed
GimGimnim <111288662+GimGimnim@users.noreply.github.com>
2022-09-19 11:32:35 +0100

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a given string
+ * @s: String to reverse
+ */
+void rev_string(char *s)
+{
+	int i, c = 0;
+	char str[2048];
+
+	while (s[c] != '\0')
+		c++;
+
+	for (i = 0; i < c; i++)
+		str[i] = s[i];
+
+	for (i = 0; i < c; i++)
+		s[i] = str[c - 1 - i];
+}
