fc244819bda38ee6e168b1d6fdf0ce076b232186
GimGimnim <111288662+GimGimnim@users.noreply.github.com>
2022-09-19 11:33:50 +0100

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string to be printed
+ */
+void puts_half(char *str)
+{
+	int i, h, c = 0;
+
+	while (str[c] != '\0')
+		c++;
+
+	if (c % 2 != 0)
+		h = (c - 1) / 2;
+	else
+		h = c / 2;
+
+	for (i = h; i > 0; i--)
+	{
+		_putchar(str[c - i]);
+	}
+
+	_putchar('\n');
+}
