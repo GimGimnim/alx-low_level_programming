0fd7ac3943a68b74482e5055137732cd33ea6145
GimGimnim <111288662+GimGimnim@users.noreply.github.com>
2022-09-19 11:34:31 +0100

    Create 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - Prints n elements from an array
+ * @a: array to print from
+ * @n: number of elements to print
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n - 1; i++)
+	{
+		printf("%d, ", a[i]);
+	}
+
+	if (n > 0)
+		printf("%d\n", a[n - 1]);
+	else
+		printf("\n");
+}
