104158e3b1ad3893847e163c70a25eb1558b12fe
GimGimnim <111288662+GimGimnim@users.noreply.github.com>
2022-09-21 14:04:38 +0100

    Greatest

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,3 +1,22 @@
 #include "main.h"
 
+/**
+ * reverse_array - reverses elements in an array
+ * @a: array to reverse
+ * @n: number of elements in the array
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int arr[2048];
 
+	for (i = 0; i < n; i++)
+	{
+		arr[i] = a[i];
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		a[i] = arr[n - 1 - i];
+	}
+}
