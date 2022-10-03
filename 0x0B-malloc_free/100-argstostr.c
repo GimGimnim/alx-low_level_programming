c5271bc950367b8b40ac348ea154a604e2ec4450
GimGimnim <111288662+GimGimnim@users.noreply.github.com>
2022-10-03 23:36:27 +0100

    Greatest

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concats all args
+ * @ac: num of args
+ * @av: args
+ *
+ * Return: pointer to new string
+ */
+char *argstostr(int ac, char **av)
+{
+	char *s;
+	int i, j, k, count = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			count++;
+		}
+	}
+
+	s = malloc(sizeof(char) * (count + ac + 1));
+
+	if (s == NULL)
+		return (NULL);
+
+	k = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			s[k] = av[i][j];
+			k++;
+		}
+		s[k] = '\n';
+		k++;
+	}
+
+	s[k] = '\0';
+
+	return (s);
+}
