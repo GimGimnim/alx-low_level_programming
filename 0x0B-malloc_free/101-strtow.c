d981406b9ab168620a8f3d9496d0c953f9b15276
GimGimnim <111288662+GimGimnim@users.noreply.github.com>
2022-10-03 23:37:06 +0100

    Greatest

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * strtow - splits a string into an array of words
+ * @str: string to split
+ *
+ * Return: pointer to array
+ */
+char **strtow(char *str)
+{
+	int i, j, k = 0;
+	int nw = 0;
+	char **p;
+
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
+			nw++;
+	}
+
+	if (nw == 0)
+		return (NULL);
+	p = malloc(sizeof(char *) * (nw + 1));
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < nw; i++)
+	{
+		while (str[k] == ' ')
+			k++;
+
+		for (j = 0; str[k] != ' ' && str[k] != '\0'; k++)
+			j++;
+
+		k -= j;
+		p[i] = malloc(sizeof(char) * (j + 1));
+
+		if (p[i] == NULL)
+			return (NULL);
+
+		for (j = 0; str[k] != ' ' && str[k] != '\0'; j++, k++)
+			p[i][j] = str[k];
+
+		p[i][j] = '\0';
+	}
+	p[nw] = NULL;
+	return (p);
+}
