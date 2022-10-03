04a1a0440868abba0055de775d492d11695f3621
GimGimnim <111288662+GimGimnim@users.noreply.github.com>
2022-10-03 23:33:36 +0100

    Greatest

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - creates a new spot in memory containeing a copy of a given string
+ * @str: string to copy
+ *
+ * Return: pointer to new string
+ */
+char *_strdup(char *str)
+{
+	unsigned int i, len = 0;
+	char *p;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
+
+	len++;
+
+	p = malloc(sizeof(char) * len);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+	{
+		p[i] = str[i];
+	}
+
+	p[i] = '\0';
+
+	return (p);
+}
