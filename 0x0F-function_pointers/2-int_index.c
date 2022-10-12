a9a263bf2a417a64b3d0dae3b578eb57a9948eba
GimGimnim <111288662+GimGimnim@users.noreply.github.com>
2022-10-13 00:30:15 +0100

    Greatest

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,26 +1,25 @@
-#include <stdlib.h>
 #include "function_pointers.h"
-
 /**
- * int_index - searches for an integer in an array
- * @array: array to search through
- * @size: size of array
- * @cmp: function to compare values
- *
- * Return: index of the first occurance of the int
- */
+* int_index - function
+* @array: array of integers to search
+* @size: size of array
+* @cmp: ptr to function int_index
+*
+* Description: function to search for an integer in an array
+* Return: -1 if does not match
+*/
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-  if (array == NULL || cmp == NULL)
+	if (cmp == NULL || array == NULL || size <= 0)
+	{
 		return (-1);
-
-  for (i = 0; i < size; i++)
+	}
+	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
 			return (i);
 	}
-
 	return (-1);
 }
