4a20ee6b19b423315c8391212d921c953caa7f0b
GimGimnim <111288662+GimGimnim@users.noreply.github.com>
2022-10-12 23:42:46 +0100

    Greatest

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a given function on every element of a given array
+ * @array: array to iterate through
+ * @size: size of array
+ * @action: function to execute
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+  for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
